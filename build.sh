c92f02c5782844c7eb5edb3dcd6738af42edb930
LuYaHu <1210797561@qq.com>
2025-01-14 18:45:33 +0800

    完成opensbi移植

--- build.sh
+++ build.sh
@@ -25,3 +25,30 @@ cd  $SHELL_FOLDER/output/lowlevelboot
 rm -rf fw.bin
 dd of=fw.bin bs=1k count=32k if=/dev/zero
 dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
+
+#编译 opensbi
+if [ ! -d "$SHELL_FOLDER/output/opensbi" ]; then  
+    mkdir $SHELL_FOLDER/output/opensbi
+fi  
+cd $SHELL_FOLDER/opensbi-1.2
+make CROSS_COMPILE=$CROSS_PREFIX- PLATFORM=quard_star
+cp -r $SHELL_FOLDER/opensbi-1.2/build/platform/quard_star/firmware/*.bin $SHELL_FOLDER/output/opensbi/
+
+# 生成 sbi.dtb
+cd $SHELL_FOLDER/dts
+dtc -I dts -O dtb -o $SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb quard_star_sbi.dts
+
+# 合成 firmware 固件
+if [ ! -d "$SHELL_FOLDER/output/fw" ]; then  
+    mkdir $SHELL_FOLDER/output/fw
+fi  
+cd $SHELL_FOLDER/output/fw
+rm -rf fw.bin
+# 填充 32K 的 0
+dd of=fw.bin bs=1k count=32k if=/dev/zero   
+# 写入 lowlevel_fw.bin 偏移量地址为 0
+dd of=fw.bin bs=1k conv=notrunc seek=0 if=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
+# 写入 quard_star_sbi.dtb 地址偏移量为 512K，因此 fdt 的地址偏移量为 0x80000
+dd of=fw.bin bs=1k conv=notrunc seek=512 if=$SHELL_FOLDER/output/opensbi/quard_star_sbi.dtb
+# 写入 fw_jump.bin 地址偏移量为 2K*1K= 0x2000000，因此 fw_jump.bin 的地址偏移量为  0x2000000
+#
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-DEFAULT_VC="1920x1080"
+DEFAULT_VC="1080x1080"
+
 $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -M quard-star \
 -m 1G \
 -smp 8 \
 -bios none \
--drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/lowlevelboot/fw.bin \
+-drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/fw/fw.bin \
+-d in_asm -D qemu.log \
 --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --serial vc:$DEFAULT_VC --monitor vc:$DEFAULT_VC --parallel none \
-#-nographic --parallel none
+#-nographic --parallel none \
